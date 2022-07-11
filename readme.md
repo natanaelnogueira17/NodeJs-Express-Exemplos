36482a4dddc9d9b33d27636abee8b044c56dea96
natanael nogueira <natanaelnogueira019@gmail.com>
2022-07-11 19:02:38 -0300

    criando rotas

--- readme.md
+++ readme.md
@@ -3,3 +3,7 @@
 - instalar todas as dependencias com `npm i`
 -instalar o webpack globalmente com `npm i -g webpack@5.68.0`
 - Rodar localmente usando `npm run dev`
+
+- sites para consulta de verbos 
+https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Methods
+https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status
--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
-const express = require ('express')
+import * as express from 'express'
+
 
 const app = express()
 
+
+
 app.get('/',(req, res)=>{
-  res.send('Hello World')
+  res.send('GET/')
 })
+
+app.post('/', (req, res)=>{
+  res.send('POST/')
+  })
+
+  app.put('/', (req, res)=>{
+    res.send('PUT/')
+    })
+    
+  
+app.delete('/', (req, res)=>{
+res.send('DELETE')
+})
+
+
+app.patch('/', (req, res)=>{
+  res.send('PATCH/')
+  })
+
+
 
 app.listen(3000, ()=> console.log('Online http://localhost:3000'))
