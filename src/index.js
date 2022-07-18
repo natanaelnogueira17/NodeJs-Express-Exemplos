03a9d106b2dd46b859fd041d7d3595de4826ea97
natanael nogueira <natanaelnogueira019@gmail.com>
2022-07-17 23:33:42 -0300

    iniciando com NodeJS

--- src/index.js
+++ src/index.js
@@ -1,32 +1,13 @@
-import * as express from 'express'
-
+const express = require('express')
 
 const app = express()
 
-
-
-app.get('/',(req, res)=>{
-  res.send('GET/')
+app.get('/', function(req, res){
+  res.send('hello world')
 })
 
-app.post('/', (req, res)=>{
-  res.send('POST/')
-  })
-
-  app.put('/', (req, res)=>{
-    res.send('PUT/')
-    })
-    
-  
-app.delete('/', (req, res)=>{
-res.send('DELETE')
+app.get('/teste', function(req, res){
+  res.send('hello Teste')
 })
 
-
-app.patch('/', (req, res)=>{
-  res.send('PATCH/')
-  })
-
-
-
-app.listen(3000, ()=> console.log('Online http://localhost:3000'))
+app.listen(3000, ()=> console.log(`ONLINE http://localhost:3000`))
